1bfc530cd5e4a337b7aed2d8e2a756e23dd1f513
LIQingQing666 <2869636206@qq.com>
2026-01-30 23:36:39 +0800

    远程调用基本实现

--- model/src/main/java/com/atguigu/order/bean/Order.java
+++ model/src/main/java/com/atguigu/order/bean/Order.java
@@ -1,19 +1,18 @@
 package com.atguigu.order.bean;
 
+import com.atguigu.product.bean.Product;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
-@Component
 public class Order {
     private Long id;
     private BigDecimal totalAmount;
     private Long userId;
     private String nickName;
     private String address;
-    private List<Object> productlist;
+    private List<Product> productlist;
 
 }
--- services/service-order/src/main/java/com/atguigu/order/service/impl/OrderServiceImpl.java
+++ services/service-order/src/main/java/com/atguigu/order/service/impl/OrderServiceImpl.java
@@ -1,26 +1,65 @@
 package com.atguigu.order.service.impl;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 import com.atguigu.order.bean.Order;
 import com.atguigu.order.service.OrderService;
+import com.atguigu.product.bean.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class OrderServiceImpl implements OrderService {
 
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 创建订单,并远程调用商品服务查询商品信息
+     * @param productId
+     * @param userId
+     * @return
+     */
     @Override
     public Order createOrder(Long productId, Long userId) {
         Order order = new Order();
-
+        Product product = getProductFromRemote(productId);
         order.setId(1L);
-        //TODO: 远程查询商品信息
-        order.setTotalAmount(new BigDecimal("0"));
+        //总金额
+        order.setTotalAmount(product.getPrice().multiply(new BigDecimal(product.getNum())));
         order.setUserId(userId);
         order.setNickName("lqq");
         order.setAddress("xian");
-        //TODO: 远程查询商品列表
-        order.setProductlist(null);
+        //商品列表
+        order.setProductlist(Arrays.asList( product));
 
         return order;
+    }
+
+    /**
+     * 根据商品信息远程调用商品服务
+     * @param productId
+     * @return
+     */
+    private Product getProductFromRemote(Long productId) {
+        //1.获取到商品服务所在的所有机器ip+port
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+        //在list中随机取一个ip+port，这里获取第一个实例
+        ServiceInstance instance = instances.get(0);
+        //远程url
+         String url = "http://" +instance.getHost() + ":" + instance.getPort() + "/product/" + productId;
+         //2.给远程发送请求
+        log.info("远程请求，路径是url:{}",url);
+        Product forObject = restTemplate.getForObject(url, Product.class);
+        return forObject;
     }
 }
