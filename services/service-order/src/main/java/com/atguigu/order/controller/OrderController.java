dc1b4ee9c4d336e1a867e5f9d4ec5649b6fe675a
LIQingQing666 <2869636206@qq.com>
2026-01-31 13:09:22 +0800

    配置中心-动态刷新

--- services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.atguigu.order.controller;
 
 import com.atguigu.order.bean.Order;
+import com.atguigu.order.properties.OrderProperties;
 import com.atguigu.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,16 +11,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope//导入配置中心时，自动刷新配置
+//@RefreshScope//导入配置中心时，自动刷新配置
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
-    @Value("${order.timeout}")
-    String orderTimeout ;
-    @Value("${order.auto-confirm}")
-    String orderAutoConfirm;
+   @Autowired
+    OrderProperties orderProperties;
+
+//    @Value("${order.timeout}")
+//    String orderTimeout ;
+//    @Value("${order.auto-confirm}")
+//    String orderAutoConfirm;
 
     /**
      * 返回获取配置的值
@@ -27,7 +31,7 @@ public class OrderController {
      */
     @GetMapping("/config")
     public String config(){
-        return "order.timeout:" + orderTimeout + " order.auto-confirm:" + orderAutoConfirm;
+        return "order.timeout:" + orderProperties.getTimeout() + " order.auto-confirm:" + orderProperties.getAutoConfirm();
     }
     /**
      * 创建订单
---
+++ services/service-order/src/main/java/com/atguigu/order/properties/OrderProperties.java
@@ -0,0 +1,13 @@
+package com.atguigu.order.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "order")//配置批量绑定在nacos下，可以无需@RefreshScope就可实现
+public class OrderProperties {
+    String Timeout ;
+    String AutoConfirm;
+}
