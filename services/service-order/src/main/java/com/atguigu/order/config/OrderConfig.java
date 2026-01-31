9a5a636c9588195009daf125146327c315ee1fcd
LIQingQing666 <2869636206@qq.com>
2026-01-31 20:13:00 +0800

    OpenFeign-进阶配置-日志

--- services/service-order/src/main/java/com/atguigu/order/config/OrderConfig.java
+++ services/service-order/src/main/java/com/atguigu/order/config/OrderConfig.java
@@ -1,11 +1,17 @@
 package com.atguigu.order.config;
 
+import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OrderConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
